c1cbb509fbeff4b28806a09e1c1152e51eaf6603
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-03-09 17:26:34 -0500

    Creating reducers for counting actions.

---
+++ src/state/slices/CounterSlice.js
@@ -0,0 +1,30 @@
+/* Author: Sebastian Aguirre Duque
+E-mail: sadw621@gmail.com */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    counter: 10
+  },
+  reducers: {
+    increment: (state) => {
+      state.counter += 1
+    },
+    decrement: (state) => {
+      state.counter -= 1
+    },
+    incrementBy: (state, action) => {
+      state.counter += action.payload
+    },
+    decrementBy: (state, action) => {
+      state.counter -= action.payload
+    },
+  }
+})
+
+export const { increment, decrement, incrementBy, decrementBy } = counterSlice.actions;
+
+export default counterSlice.reducer;
+
