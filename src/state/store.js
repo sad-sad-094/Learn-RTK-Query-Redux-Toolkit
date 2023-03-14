53cd16b2c8dbb37c67d2c737a537dda1fdb5a350
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-03-14 16:33:58 -0500

    Adding pokemonSlice to store.

--- src/state/store.js
+++ src/state/store.js
@@ -4,9 +4,11 @@ E-mail: sadw621@gmail.com */
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./slices/CounterSlice";
+import pokemonReducer from "./slices/PokemonSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    pokemons: pokemonReducer,
   },
-})
+}) 
