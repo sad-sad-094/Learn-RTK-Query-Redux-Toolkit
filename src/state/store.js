a9b73c23f132bd4b196ba54475fb93c66a187f9b
Sebastian Aguirre Duque <sadw621@gmail.com>
2023-03-15 18:03:35 -0500

    Add ToDo API reducer from RTK Query.

--- src/state/store.js
+++ src/state/store.js
@@ -3,6 +3,7 @@ E-mail: sadw621@gmail.com */
 
 import { configureStore } from "@reduxjs/toolkit";
 
+import { toDoAPIs } from "./APIs/ToDoAPIs";
 import counterReducer from "./slices/CounterSlice";
 import pokemonReducer from "./slices/PokemonSlice";
 
@@ -10,5 +11,10 @@ export const store = configureStore({
   reducer: {
     counter: counterReducer,
     pokemons: pokemonReducer,
+
+    [toDoAPIs.reducerPath]: toDoAPIs.reducer,
   },
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(toDoAPIs.middleware)
+  
 }) 
